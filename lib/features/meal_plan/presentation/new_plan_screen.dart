10664ccf49a1a99b6832d5934a2542631436914b
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2024-11-25 19:00:14 +0100

    upload images screen

--- lib/features/meal_plan/presentation/new_plan_screen.dart
+++ lib/features/meal_plan/presentation/new_plan_screen.dart
@@ -10,6 +10,97 @@ class NewPlanScreen extends StatefulWidget {
 class _NewPlanScreenState extends State<NewPlanScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Neuer Plan",
+                    style: textTheme.displayMedium,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        "Zeige uns deine Vorräte",
+                        style: textTheme.titleLarge,
+                        textAlign: TextAlign.start,
+                      ),
+                      SizedBox(width: 20),
+                      IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Zeige uns deine Vorräte"),
+                                  content: Text(
+                                    "Erstelle Fotos von deinen Vorräten wie zum Beispiel deinem Kühlschrank oder Vorratsschrank und MealMaster erstellt dir einen passenden Wochenplan.",
+                                    style: textTheme.bodyLarge,
+                                    softWrap: true,
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(); // Dialog schließen
+                                      },
+                                      child: Text("Schließen"),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          icon: Icon(Icons.info_outline))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Card.filled(),
+                      ),
+                      SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Card.filled(),
+                      ),
+                      SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Card.filled(),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  FilledButton.icon(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      //TODO: Implement upload images
+                    },
+                    label: Text("Vorräte hinzufügen"),
+                  ),
+                ],
+              ),
+              FilledButton(
+                onPressed: null,
+                child:
+                Text("Weiter"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
