9736e8cb1b112e215365796228b6d78ad68f1dc8
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2024-12-09 12:30:31 +0100

    test data

--- lib/features/recipes/presentation/recipe_screen.dart
+++ lib/features/recipes/presentation/recipe_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../../common/widgets/base_scaffold.dart';
+import '../../../db/ingredient.dart';
+import '../../../db/meal_plan.dart';
+import '../../../db/meal_plan_entry.dart';
+import '../../../db/recipe.dart';
+import '../../../db/recipe_ingredient.dart';
+import '../../../db/recipe_step.dart';
 
 class RecipeScreen extends StatefulWidget {
   final int id;
@@ -12,12 +18,53 @@ class RecipeScreen extends StatefulWidget {
 }
 
 class _RecipeScreenState extends State<RecipeScreen> {
+  Recipe getTestRecipe() {
+    final Ingredient ingredient = Ingredient()
+      ..name = "Milch"
+      ..unit = "ml";
+
+    final RecipeIngredient recipeIngredient = RecipeIngredient()
+      ..count = 3
+      ..ingredients.add(ingredient);
+
+    final RecipeStep recipeStep = RecipeStep()
+      ..description = "Schritt 1 Milch milchig machen"
+      ..orderPosition = 1;
+
+    final MealPlan mealPlan = MealPlan()
+      ..startDate = DateTime.now()
+      ..endDate = DateTime.now().add(Duration(days: 7));
+
+    final MealPlanEntry mealPlanEntry = MealPlanEntry()
+      ..day = DateTime.now()
+      ..mealPlan.add(mealPlan);
+
+    final Recipe recipe = Recipe()
+      ..cookingDuration = 25
+      ..difficulty = 2
+      ..steps.add(recipeStep)
+      ..mealPlanEntry.add(mealPlanEntry)
+      ..ingredients.add(recipeIngredient);
+
+    return recipe;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Recipe recipe = getTestRecipe();
+
     return BaseScaffold(
       title: "Rezept ${widget.id}",
       hasBackButton: true,
-      child: Text('Rezept'),
+      child: Column(
+        children: [
+          Text('Rezept'),
+          Text('Kochdauer: ${recipe.cookingDuration} Minuten'),
+          Text('Schwierigkeit: ${recipe.difficulty}'),
+          Text('Zutaten:'),
+          Text('Schritte:'),
+        ],
+      ),
     );
   }
 }
