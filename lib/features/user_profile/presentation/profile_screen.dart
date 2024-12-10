22aa642b5cbc8a3682cdc63e79e7274ecd84db22
Tim Oliver Wolter <timoliver20o11@gmail.com>
2024-12-10 18:58:48 +0100

    added Diet Enum

--- lib/features/user_profile/presentation/profile_screen.dart
+++ lib/features/user_profile/presentation/profile_screen.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+enum DietLabel {
+  noDiet("Keine Besonderheiten"),
+  halal("Halal"),
+  kosher("Kosher"),
+  vegan("Vegan"),
+  vegetarian("Vegetarisch");
+
+  const DietLabel(this.label);
+  final String label;
+}
+
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
@@ -8,8 +19,38 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final TextEditingController dietController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              "MealMe",
+              textScaler: TextScaler.linear(2),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Dein Name"),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Gewicht"),
+            ),
+            DropdownMenu(
+                initialSelection: DietLabel.noDiet,
+                controller: dietController,
+                label: const Text("Ernährungsform"),
+                dropdownMenuEntries: DietLabel.values
+                    .map<DropdownMenuEntry<DietLabel>>((DietLabel diet) {
+                  return DropdownMenuEntry<DietLabel>(
+                      value: diet, label: diet.label);
+                }).toList()),
+          ],
+        ),
+      ),
+    );
   }
 }
