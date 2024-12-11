1a015e426cedccf0c3e928557fac238f12049b63
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2024-12-11 17:25:15 +0100

    format recipe

--- lib/features/recipes/presentation/recipe_screen.dart
+++ lib/features/recipes/presentation/recipe_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../common/widgets/base_scaffold.dart';
-import '../../../db/ingredient.dart';
-import '../../../db/meal_plan.dart';
-import '../../../db/meal_plan_entry.dart';
 import '../../../db/recipe.dart';
-import '../../../db/recipe_ingredient.dart';
-import '../../../db/recipe_step.dart';
+import '../../home/presentation/widgets/difficulty_indicator.dart';
 
 class RecipeScreen extends StatefulWidget {
   final int id;
@@ -19,32 +15,9 @@ class RecipeScreen extends StatefulWidget {
 
 class _RecipeScreenState extends State<RecipeScreen> {
   Recipe getTestRecipe() {
-    final Ingredient ingredient = Ingredient()
-      ..name = "Milch"
-      ..unit = "ml";
-
-    final RecipeIngredient recipeIngredient = RecipeIngredient()
-      ..count = 3
-      ..ingredients.add(ingredient);
-
-    final RecipeStep recipeStep = RecipeStep()
-      ..description = "Schritt 1 Milch milchig machen"
-      ..orderPosition = 1;
-
-    final MealPlan mealPlan = MealPlan()
-      ..startDate = DateTime.now()
-      ..endDate = DateTime.now().add(Duration(days: 7));
-
-    final MealPlanEntry mealPlanEntry = MealPlanEntry()
-      ..day = DateTime.now()
-      ..mealPlan.add(mealPlan);
-
     final Recipe recipe = Recipe()
       ..cookingDuration = 25
-      ..difficulty = 2
-      ..steps.add(recipeStep)
-      ..mealPlanEntry.add(mealPlanEntry)
-      ..ingredients.add(recipeIngredient);
+      ..difficulty = 2;
 
     return recipe;
   }
@@ -54,6 +27,8 @@ class _RecipeScreenState extends State<RecipeScreen> {
     final Recipe recipe = getTestRecipe();
     final recipeCount = 5;
 
+    final theme = Theme.of(context).textTheme;
+
     return BaseScaffold(
       title: "Rezept ${widget.id}",
       hasBackButton: true,
@@ -61,8 +36,10 @@ class _RecipeScreenState extends State<RecipeScreen> {
         padding: const EdgeInsets.symmetric(vertical: 20.0),
         child: Column(
           children: [
+            Text("Day", style: theme.displaySmall),
+            SizedBox(height: 20),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 2 / 3, // 2/3
+              height: MediaQuery.of(context).size.height * 2 / 3,
               child: PageView.builder(
                 controller: PageController(
                   viewportFraction: 0.8,
@@ -77,18 +54,31 @@ class _RecipeScreenState extends State<RecipeScreen> {
                         color: Theme.of(context).primaryColorLight,
                         borderRadius: BorderRadius.circular(12.0),
                       ),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Bild $index',
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Text('Rezept'),
-                          Text('Kochdauer: ${recipe.cookingDuration} Minuten'),
-                          Text('Schwierigkeit: ${recipe.difficulty}'),
-                          Text('Zutaten: ${recipe.ingredients.length}'),
-                          Text('Schritte: ${recipe.steps.length}'),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    '${recipe.cookingDuration} min',
+                                    style: theme.titleMedium,
+                                  ),
+                                  SizedBox(width: 20),
+                                  Text('-'),
+                                  SizedBox(width: 20),
+                                  DifficultyIndicator(
+                                    difficulty: recipe.difficulty!,
+                                    size: 20,
+                                  ),
+                                ]),
+                            Text('Zutaten', style: theme.headlineMedium),
+                            SizedBox(height: 20),
+                            Text('Schritte:', style: theme.headlineMedium),
+                          ],
+                        ),
                       ),
                     ),
                   );
