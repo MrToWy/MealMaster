23922c95b78ccd7415c94cf6ef2b10958cb4c26b
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2024-12-18 15:07:41 +0100

    recipe repository

--- lib/features/meal_plan/data/meal_plan_repository.dart
+++ lib/features/meal_plan/data/meal_plan_repository.dart
@@ -1,1 +1,39 @@
+import 'package:isar/isar.dart';
 
+import '../../../db/isar_factory.dart';
+import '../../../db/meal_plan.dart';
+import '../../../db/meal_plan_entry.dart';
+
+class MealPlanRepository {
+  static final MealPlanRepository _instance = MealPlanRepository._internal();
+  late Future<Isar> isarInstance = IsarFactory().db;
+
+  factory MealPlanRepository() {
+    return _instance;
+  }
+
+  MealPlanRepository._internal();
+
+  Future<MealPlan> getCurrentMealPlan() async {
+    final isar = await isarInstance;
+    final mealPlan =
+        await isar.mealPlans.where().sortByStartDateDesc().findFirst();
+
+    if (mealPlan == null) {
+      throw Exception("No meal plan found");
+    }
+
+    return mealPlan;
+  }
+
+  Future<List<MealPlanEntry>> getMealPlanEntries(MealPlan mealPlan) async {
+    await mealPlan.entries.load();
+    final entries = mealPlan.entries.toList();
+
+    if (entries.isEmpty) {
+      throw Exception("No entries found for meal plan");
+    }
+
+    return entries;
+  }
+}
--- lib/features/recipes/presentation/recipe_screen.dart
+++ lib/features/recipes/presentation/recipe_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../common/widgets/base_scaffold.dart';
 import '../../../db/recipe.dart';
 import '../../home/presentation/widgets/difficulty_indicator.dart';
+import '../data/recipe_repository.dart';
 
 class RecipeScreen extends StatefulWidget {
   final int id;
@@ -14,39 +15,48 @@ class RecipeScreen extends StatefulWidget {
 }
 
 class _RecipeScreenState extends State<RecipeScreen> {
-  Recipe getTestRecipe() {
-    final Recipe recipe = Recipe()
-      ..cookingDuration = 25
-      ..difficulty = 2;
+  List<Recipe> recipes = [];
+  Recipe currentRecipe = Recipe();
 
-    return recipe;
+  @override
+  initState() {
+    getRecipes();
+    super.initState();
+  }
+
+  getRecipes() async {
+    final recipes = await RecipeRepository().getRecipes();
+    final recipe = await RecipeRepository().getRecipeById(widget.id);
+    setState(() {
+      this.recipes = recipes;
+      currentRecipe = recipe;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final Recipe recipe = getTestRecipe();
-    final recipeCount = 5;
-
     final theme = Theme.of(context).textTheme;
 
     return BaseScaffold(
-      title: "Rezept ${widget.id}",
+      title: "${currentRecipe.title}",
       hasBackButton: true,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 20.0),
         child: Column(
           children: [
-            Text("Day", style: theme.displaySmall),
+            Text("DAY", style: theme.displaySmall),
             SizedBox(height: 20),
             SizedBox(
               height: MediaQuery.of(context).size.height * 2 / 3,
               child: PageView.builder(
                 controller: PageController(
                   viewportFraction: 0.8,
-                  initialPage: widget.id,
+                  initialPage: 0, // TODO set to current recipe index
                 ),
-                itemCount: recipeCount,
+                itemCount: recipes.length,
                 itemBuilder: (context, index) {
+                  Recipe recipe = recipes[index];
+
                   return Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     child: Container(
@@ -70,7 +80,8 @@ class _RecipeScreenState extends State<RecipeScreen> {
                                   Text('-'),
                                   SizedBox(width: 20),
                                   DifficultyIndicator(
-                                    difficulty: recipe.difficulty!,
+                                    difficulty: 2,
+                                    // TODO set to recipe difficulty
                                     size: 20,
                                   ),
                                 ]),
