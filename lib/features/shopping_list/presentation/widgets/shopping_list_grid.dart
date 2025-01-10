ca62815d06b8f3b0d904f5d764dee159493877be
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2025-01-10 17:09:33 +0100

    style shopping list

--- lib/features/shopping_list/presentation/widgets/shopping_list_grid.dart
+++ lib/features/shopping_list/presentation/widgets/shopping_list_grid.dart
@@ -4,25 +4,36 @@ import '../../../../db/shopping_list_entry.dart';
 import 'add_shopping_list_item_button.dart';
 import 'shopping_list_item_card.dart';
 
-Widget shoppingListGrid(List<ShoppingListEntry> shoppingList, bool hasAddButton,
-    Function onClick, Function callBack) {
-  int startIndex = hasAddButton ? 1 : 0;
+class ShoppingListGrid extends StatelessWidget {
+  final bool hasAddButton;
+  final Function onClick;
+  final Function callBack;
+  final List<ShoppingListEntry> shoppingList;
 
-  if (shoppingList.isEmpty) {
-    return Center(
-      child: Text("Du hast alles was du brauchst!"),
-    );
+  const ShoppingListGrid(
+      {super.key,
+      required this.hasAddButton,
+      required this.onClick,
+      required this.callBack,
+      required this.shoppingList});
+
+  @override
+  Widget build(BuildContext context) {
+    int startIndex = hasAddButton ? 1 : 0;
+
+    return GridView.builder(
+        itemCount: startIndex + shoppingList.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (context, index) {
+          if (index == 0 && hasAddButton) {
+            return AddShoppingListItemButton();
+          }
+
+          return ShoppingListItemCard(
+              item: shoppingList.elementAt(index - startIndex),
+              onClick: onClick,
+              callback: callBack);
+        });
   }
-
-  return GridView.builder(
-      itemCount: startIndex + shoppingList.length,
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (context, index) {
-        if (index == 0 && hasAddButton) {
-          return addShoppingListItemButton(context, callBack);
-        }
-        return shoppingListItemCard(context,
-            shoppingList.elementAt(index - startIndex), onClick, callBack);
-      });
 }
