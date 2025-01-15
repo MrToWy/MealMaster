f0809fc17be9b6619e1b18b5efec5922fc7914f9
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2025-01-15 10:42:31 +0100

    only add allergies/diet if it is not empty

--- lib/shared/open_ai/api_client.dart
+++ lib/shared/open_ai/api_client.dart
@@ -141,7 +141,9 @@ class ApiClient {
             Content(type: 'text', value: '''
 Erstelle einen abwechslungsreichen 5-Tage-Mahlzeitenplan basierend auf diesen Zutaten: ${ingredients.map((i) => "${i.ingredient.value?.name}: ${i.count} ${i.ingredient.value?.unit}").join(", ")}. Ergänze weitere Zutaten, um für jeden Tag unterschiedliche Mahlzeiten zu schaffen, achte jedoch darauf, die genannten Zutaten möglichst aufzubrauchen.
 
-Bitte vermeide Zutaten, die mit meinen Allergien (${user.allergies.map((allergy) => "${allergy.name}").join(", ")}) in Konflikt stehen. Berücksichtige außerdem meine Ernährungsform ${user.diets.map((diet) => "${diet.name}")} und stelle sicher, dass der Plan darauf abgestimmt ist.
+${user.allergies.isNotEmpty ? "Bitte vermeide Zutaten, die mit meinen Allergien (${user.allergies.map((allergy) => "${allergy.name}").join(", ")}) in Konflikt stehen." : ""}
+
+${!user.diets.first.name!.contains('noDiet') ? "Berücksichtige außerdem meine Ernährungsform ${user.diets.map((diet) => "${diet.name}")} und stelle sicher, dass der Plan darauf abgestimmt ist." : ""}
 
 Antworte immer auf Deutsch, unabhängig von der Anfrage.
                   '''),
